779adefda703becfe60be1cfdc4d18115cf165bc
Christoph Trautwein <ch.trautwein@gmail.com>
2022-09-06 16:17:56 +0200

    predict

---
+++ ml/9138/pea/handler.py
@@ -0,0 +1,82 @@
+from catboost import CatBoostClassifier
+import pandas as pd
+import MetaTrader5 as mt5
+
+MA_PERIODS = [i for i in range(15, 500, 15)]
+
+def update_rates(old_df, count = 10) -> pd.DataFrame:
+    # Update
+
+    if not mt5.initialize():
+        print("initialize() failed")
+        mt5.shutdown()
+
+    rates = mt5.copy_rates_from_pos("EURUSD", mt5.TIMEFRAME_M5, 0, count)
+    mt5.shutdown()
+
+    print(rates)
+
+    rates_frame = pd.DataFrame(columns=['time', 'close'])
+    rates_frame['time'] = pd.to_datetime(
+        rates['time'] * 1000 * 1000 * 1000)
+    rates_frame['close'] = rates['close']
+    rates_frame.set_index('time', inplace=True)
+    rates_frame.index = pd.to_datetime(rates_frame.index, unit='s')
+
+    print('From mt5')
+    print(rates_frame.tail())
+    return pd.concat([old_df, rates_frame]).dropna().drop_duplicates()
+
+def calc_features(ds) -> pd.Series:
+    current_row = ds.iloc[-1]
+    dsC = ds.copy()
+    f = pd.Series([current_row['close']],dtype = float)
+
+    count = 0
+    for i in MA_PERIODS:
+        # f[str(count)] = dsC - dsC.rolling(i).mean()
+        m = dsC - dsC.rolling(i).mean()
+        mean = m.iloc[-1]['close']
+        f = pd.concat([f,pd.Series([mean])], ignore_index=True)
+        count += 1
+    print('features')
+    print(f)
+    return f
+
+def initDataset() -> pd.DataFrame:
+    return pd.DataFrame()
+
+def newBar():
+    global dataset
+    dataset = update_rates(dataset)
+    print('Rates updated')
+    print(dataset)
+    features = calc_features(dataset)
+    signal = model.predict(features)
+    meta_signal = meta_model.predict(features)
+    return signal, meta_signal
+
+
+model = CatBoostClassifier(iterations=1000,
+                               depth=6,
+                               learning_rate=0.1,
+                               custom_loss=['Accuracy'],
+                               eval_metric='Accuracy',
+                               verbose=False,
+                               use_best_model=True,
+                               task_type='CPU')
+model.load_model("catmodel.cbm")
+
+meta_model = CatBoostClassifier(iterations=1000,
+                                    depth=6,
+                                    learning_rate=0.1,
+                                    custom_loss=['Accuracy'],
+                                    eval_metric='Accuracy',
+                                    verbose=False,
+                                    use_best_model=True,
+                                    task_type='CPU')
+meta_model.load_model("meta_catmodel.cbm")
+
+dataset = initDataset()
+dataset = update_rates(dataset, count=4000)
+   
--- ml/9138/pea/server.py
+++ ml/9138/pea/server.py
@@ -1,49 +1,32 @@
+from datetime import datetime
 from flask import Flask
 from flask import request
 import json
 import numpy as np
 from sklearn.linear_model import LinearRegression
-from catboost import CatBoostClassifier
 import pandas as pd
+import MetaTrader5 as mt5
+from handler import newBar
 
-MA_PERIODS = [i for i in range(15, 500, 15)]
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
-
-#/*
-#void fill_arays1(double & features[]) {
-#    double pr[], ret[]
-#    ArrayResize(ret, 1)
-#    for (int i=ArraySize(MAs1)-1
-#         i >= 0
-#         i--) {
-#        CopyClose(NULL, PERIOD_CURRENT, 1, MAs1[i], pr)
-#        double mean = MathMean(pr)
-#        double std = MathStandardDeviation(pr)
-#        ret[0] = pr[MAs1[i]-1] - mean
-#        ArrayInsert(features, ret, ArraySize(features), 0, WHOLE_ARRAY)
-#    }
-#    ArraySetAsSeries(features, true)
-#}
-#*/
-
-
-def get_prices() -> pd.DataFrame:
-    p = pd.read_csv('../EURUSDMT5.csv', delim_whitespace=True)
-    pFixed = pd.DataFrame(columns=['time', 'close'])
-    pFixed['time'] = p['<DATE>'] + ' ' + p['<TIME>']
-    pFixed['time'] = pd.to_datetime(pFixed['time'], infer_datetime_format=True)
-    pFixed['close'] = p['<CLOSE>']
-    pFixed.set_index('time', inplace=True)
-    pFixed.index = pd.to_datetime(pFixed.index, unit='s')
-    pFixed = pFixed.dropna()
-    pFixedC = pFixed.copy()
-
-    count = 0
-    for i in MA_PERIODS:
-        pFixed[str(count)] = pFixedC - pFixedC.rolling(i).mean()
-        count += 1
-    return pFixed.dropna()
 
 def calcregr(chartdata):
     Y = np.array(chartdata).reshape(-1,1)
@@ -58,56 +41,27 @@ def calcregr(chartdata):
 
 @app.route("/Predict", methods=['GET', 'POST'])
 def indx():
-    global prices
     if request.method == 'POST':
         if request.data:
             rcv_data = json.loads(request.data.decode(encoding='utf-8'))
-            # print(rcv_data['clprArray'])
-            # print(rcv_data)
             if rcv_data['isNewBar']:
                 print(rcv_data)
+                signal, meta_signal = newBar()
+            else:
+                signal = 2
+                meta_signal = 2
             left, right = calcregr(rcv_data['clprArray'])
-            now = pd.to_datetime(
+            tickTime = pd.to_datetime(
                 rcv_data['tickTime'], infer_datetime_format=True)
-
-            d = {'time': [now], 'close': [rcv_data['ask']]}
-            newLine = pd.DataFrame(data = d)            
-            newLine.set_index('time', inplace=True)
-            newLine.index = pd.to_datetime(newLine.index, unit='s')
-
-            # print("Recalc")
-            count = 0
-            for i in MA_PERIODS:
-                # print(prices['close'].iloc[-i:].mean())
-                newLine[str(count)] = newLine['close'] - \
-                    prices['close'].iloc[-i:].mean()
-                count += 1
-
-            # print("NewLine 1")
-            # print(newLine)
-            # print("Prices 0")
-            # print(prices)
-            prices = pd.concat([prices, newLine])
-
-            # print('Features')
-            # last row
-            features = prices.iloc[-2,:]
-            # print(features)
-            features = features.iloc[1:]
-            # print(features)
-            # features.drop('time')
-            # print(features.describe())
-            print('Prediction')
-            prediction = meta_model.predict(features)
-            print(prediction)       
 
             result = {
                 "from": left,
                 "to": right,
-                "sig": 0.1,
-                "meta_sig": 0.2
+                "sig": signal,
+                "meta_sig": meta_signal
             }
-            print(result)
+            if rcv_data['isNewBar']:
+                print(result)
             if result:
                 return json.dumps(result)
             else:
@@ -116,37 +70,10 @@ def indx():
             return '404'
 
 if __name__ == "__main__":
-    model = CatBoostClassifier(iterations=1000,
-                               depth=6,
-                               learning_rate=0.1,
-                               custom_loss=['Accuracy'],
-                               eval_metric='Accuracy',
-                               verbose=False,
-                               use_best_model=True,
-                               task_type='CPU')
-    model.load_model("catmodel.cbm")
+    print("MetaTrader5 package author: ", mt5.__author__)
+    print("MetaTrader5 package version: ", mt5.__version__)
 
-    meta_model = CatBoostClassifier(iterations=1000,
-                                    depth=6,
-                                    learning_rate=0.1,
-                                    custom_loss=['Accuracy'],
-                                    eval_metric='Accuracy',
-                                    verbose=False,
-                                    use_best_model=True,
-                                    task_type='CPU')
-    meta_model.load_model("meta_catmodel.cbm")
-
-    prices = get_prices()
-    print(prices.head())
-    print('Features')
-    # last row
-    features = prices.iloc[-2,:]
-    print(features)
-    features = features.iloc[1:]
-    print(features)
-    # features.drop('time')
-    print(features.describe())
-    print('Prediction')
-    print(meta_model.predict(features))
+    newBar()
+    newBar()
 
     app.run(host='localhost', port='43560')
