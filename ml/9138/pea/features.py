2883abd21f6c60502edbe4f086588dd1e06f44c3
Christoph Trautwein <c.trautwein@accenture.com>
2022-09-22 16:44:32 +0200

    GBPJPY

--- ml/9138/pea/features.py
+++ ml/9138/pea/features.py
@@ -1,44 +1,59 @@
 import pandas as pd
 
+FEATURE_COUNT = 0
+
 MA_PERIODS = [i for i in range(15, 500, 15)]
-SYMBOL = 'EURUSD'
-FEATURE_COUNT = len(MA_PERIODS) + 2
 
 print('Hello from features')
 
 
-def get_features(the_data) -> pd.DataFrame:
-    pTable = the_data.copy()
-    pFixedC = the_data.copy()
-    count = 0
-    for i in MA_PERIODS:
-        pTable[str(count)] = pFixedC - pFixedC.rolling(i).mean()
-        count += 1
+def calc_bollinger(the_data, feature) -> pd.DataFrame:
+    global FEATURE_COUNT
 
     # Standard deviation 20
-    pTable['std'] = pFixedC.rolling(20).std(ddof=0)
+    feature['std'] = the_data.rolling(20).std(ddof=0)
     # Moving Average 20
-    pTable['MA-TP'] = pFixedC.rolling(20).mean()
+    feature['MA-TP'] = the_data.rolling(20).mean()
     # Bollinge Band Upper
-    pTable['BOLU'] = pTable['MA-TP'] + 2*pTable['std']
+    feature['BOLU'] = feature['MA-TP'] + 2*feature['std']
     # Bollinge Band Lower
-    pTable['BOLD'] = pTable['MA-TP'] - 2*pTable['std']
+    feature['BOLD'] = feature['MA-TP'] - 2*feature['std']
 
     # above bollinger band
     # pTable['BBAbove'] = pTable['MA-TP'] > pTable['BOLU']
-    pTable['BBAbove'] = pTable['MA-TP'] - pTable['BOLU']
+    feature['BBAbove'] = feature['MA-TP'] - feature['BOLU']
+    FEATURE_COUNT += 1
 
     # below bollinger band
     # pTable['BBBelow'] = pTable['MA-TP'] < pTable['BOLD']
-    pTable['BBBelow'] = pTable['MA-TP'] - pTable['BOLD']
+    feature['BBBelow'] = feature['MA-TP'] - feature['BOLD']
+    FEATURE_COUNT += 1
 
-    pTable.drop('std', axis=1, inplace=True)
-    pTable.drop('MA-TP', axis=1, inplace=True)
-    pTable.drop('BOLU', axis=1, inplace=True)
-    pTable.drop('BOLD', axis=1, inplace=True)
+    feature.drop('std', axis=1, inplace=True)
+    feature.drop('MA-TP', axis=1, inplace=True)
+    feature.drop('BOLU', axis=1, inplace=True)
+    feature.drop('BOLD', axis=1, inplace=True)
 
-    # print(pTable)
-    # print(pTable.describe())
+    return feature
+
+
+def calc_mean(the_data, feature) -> pd.DataFrame:
+    global FEATURE_COUNT
+    count = 0
+    for i in MA_PERIODS:
+        feature[str(count)] = the_data - the_data.rolling(i).mean()
+        count += 1
+    FEATURE_COUNT += len(MA_PERIODS)
+
+    return feature
+
+
+def get_features(the_data) -> pd.DataFrame:
+    global FEATURE_COUNT
+    pTable = the_data.copy()
+
+    pTable = calc_mean(the_data, pTable)
+    pTable = calc_bollinger(the_data, pTable)
 
     return pTable
 
@@ -47,8 +62,8 @@ def get_X(df) -> pd.DataFrame:
     return df[df.columns[1:FEATURE_COUNT+1]]
 
 
-def get_historic_prices() -> pd.DataFrame:
-    p = pd.read_csv('EURUSDMT5.csv', delim_whitespace=True)
+def get_historic_prices(CSVFILE) -> pd.DataFrame:
+    p = pd.read_csv(CSVFILE, delim_whitespace=True)
     pFixed = pd.DataFrame(columns=['time', 'close'])
     pFixed['time'] = p['<DATE>'] + ' ' + p['<TIME>']
     pFixed['time'] = pd.to_datetime(pFixed['time'], infer_datetime_format=True)
